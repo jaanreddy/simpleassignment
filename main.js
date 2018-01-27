70fdd54b5408e997e33f44aa2d57bc291047a5cd
jaanreddy <jaanreddydepa@gmail.com>
2018-01-28 00:50:50 +0530

    make it better

---
+++ main.js
@@ -0,0 +1,61 @@
+	// create the module and name it greedyGameApp
+angular.module('greedyGameApp', ['ui.bootstrap']);
+
+	// create the controller and inject Angular's $scope
+	angular.module('greedyGameApp').controller('mainController', function($scope, $http, $filter) {
+          //Date options
+          $scope.fromDate= new Date(), $scope.toDate=null, $scope.response = null;
+          $scope.noData = "To fetch the data please submit valid data";
+        
+          $scope.dateOptions = {
+            formatYear: 'yyyy',
+            startingDay: 0,
+            showWeeks: false,
+            yearRange: 20,
+            allowInvalid: true,
+            datepickerMode: 'year'
+        };
+         //Decide whether list to open/close
+         $scope.fromDtOpen = function($event) {
+            $event.preventDefault();
+            $event.stopPropagation();
+            $scope.isOpen = true;
+            $scope.isToOpen = false;
+          };
+          $scope.toDtOpen = function($event) {
+            $event.preventDefault();
+            $event.stopPropagation();
+            $scope.isToOpen = true;
+            $scope.isOpen = false;
+          };
+          
+          $scope.dateChangeHandler = function(){
+            $scope.toDate=null;
+            $scope.minDate = new Date($scope.fromDate);
+            $scope.minDate.setDate($scope.minDate.getDate() + 1);
+          };
+          $scope.dateChangeHandler();
+           // function to submit the form after all validation has occurred            
+            $scope.submitForm = function(isValid) {
+                // check to make sure the form is completely valid
+                if (isValid) {
+                    //call adrequest api based on from and to dates
+                    var fDate = $filter('date')($scope.fromDate, "yyyy-MM-dd"),
+                        tDate = $filter('date')($scope.toDate, "yyyy-MM-dd");
+                    var url = "http://104.197.128.152/data/adrequests?from="+fDate+"&to="+tDate;
+                    $http.get(url)
+                    .then(function successCallback(res){
+                        if(res && res.data && res.data.data.length>0){
+                            $scope.response =  res.data.data;
+                        }else{
+                            $scope.noData = "Data not found";
+                        }
+                    }, function errorCallback(response){
+                        $scope.noData = "Unable to perform get request";
+                    });
+                }
+
+            };
+          
+	});
+
