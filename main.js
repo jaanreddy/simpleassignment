010041f285bd2dcf35abd162525c9c350a35918b
jaanreddy <jaanreddydepa@gmail.com>
2018-01-28 13:03:40 +0530

    first commit

--- main.js
+++ main.js
@@ -1,29 +1,5 @@
 
-var express = require('express');
-var app = express();
-
-// set the port of our application
-// process.env.PORT lets the port be set by Heroku
-var port = process.env.PORT || 8080;
-
-// set the view engine to ejs
-app.set('view engine', 'ejs');
-
-// make express look in the public directory for assets (css/js/img)
-app.use(express.static(__dirname + '/public'));
-
-// set the home page route
-app.get('/', function(req, res) {
-
-	// ejs render automatically looks in the views folder
-	res.render('index');
-});
-
-app.listen(port, function() {
-	console.log('Our app is running on http://localhost:' + port);
-});
-
-	// create the module and name it greedyGameApp
+// create the module and name it greedyGameApp
 angular.module('greedyGameApp', ['ui.bootstrap']);
 
 	// create the controller and inject Angular's $scope
